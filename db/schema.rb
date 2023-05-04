3413c905ab1ce8598af1c361c0bfa3bbf69a463f
James Serengia <jamesserengia@gmail.com>
2023-05-04 09:15:46 +0300

    Article resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_055437) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_061020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "cover_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sellers", force: :cascade do |t|
     t.string "name"
